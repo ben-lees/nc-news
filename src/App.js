859cc68a20d9e71d0c956f3b9ebf7e6e017f2a0c
Ben Lees <benlees@warpmail.net>
2021-11-22 20:19:16 +0000

    starts navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import NavBar from "./components/NavBar";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
-  return <div className="App"> </div>;
+  return (
+    <>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" exact />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
